41c84169039c027bf78465a04cf7693932a09bbc
LPosi <lrposada@yahoo.com.ar>
2025-09-27 20:45:30 -0300

    Estuve dándole estilos a los h1 y p del index

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-let suma = 10 + 5;
+/*let suma = 10 + 5;
 let producto = 20 * 2;
 
-if (suma<20) {
-    console.log("La suma es menor a 20");
-} else (suma>=20) {
-    console.log("La suma es mayor o igual a 20");
+if (suma < 20) {
+  console.log("La suma es menor a 20");
+} else {
+  console.log("La suma es mayor o igual a 20");
 }
+
+if (producto > 30) {
+  console.log("El producto es mayor a 30");
+}*/
+
+/*console.log("El Script está funcionando correctamente");
+
+const titulo = document.getElementById("titulo");
+titulo.textContent = "¡Texto modificado!";*/
+
+/*nombre = "Carlos";
+
+edad = 35;
+
+if (edad > 18) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
+
+console.log(edad > 18);
+console.log(nombre);*/
+
+let nombre = "Carlos";
+let edad = 35;
+let esMayor = edad > 18;
+
+if (esMayor) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Sos mayor de edad", esMayor);
