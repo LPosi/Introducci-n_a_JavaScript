51132c44856a52d0fce54a07e668a5e747b207fa
LPosi <lrposada@yahoo.com.ar>
2025-10-09 21:49:15 -0300

    Ejercios de práctica de la primer clase

--- main.js
+++ main.js
@@ -29,16 +29,72 @@ if (edad > 18) {
 console.log(edad > 18);
 console.log(nombre);*/
 
-let nombre = "Carlos";
-let edad = 35;
-let esMayor = edad > 18;
+//let nombre = "Carlos";
+//let edad = 35;
+//let esMayor = edad > 18;
 
-if (esMayor) {
+//if (esMayor) {
+//  console.log("Eres mayor de edad");
+//} else {
+//  console.log("Eres menor de edad");
+//}
+
+//console.log("Nombre:", nombre);
+//console.log("Edad:", edad);
+//console.log("Sos mayor de edad", esMayor);
+
+/* let nombre = "Valentin";
+let edad = 24;
+let ciudad = "Balcarce";
+const PI = 3.1416;
+let esMayorDeEdad = edad > 18;
+let numero1 = 3;
+let numero2 = 5;
+
+let suma = numero1 + numero2;
+let resta = numero2 - numero1;
+let multiplicacion = numero1 * numero2;
+let division = numero2 / numero1;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+
+console.log("Nombre:", nombre);
+console.log("Edad", edad);
+console.log("Sos de la ciudad de", ciudad);
+
+if (esMayorDeEdad) {
   console.log("Eres mayor de edad");
 } else {
   console.log("Eres menor de edad");
 }
+*/
+/*let numero = Number(prompt("Ingresá un número:"));
 
-console.log("Nombre:", nombre);
-console.log("Edad:", edad);
-console.log("Sos mayor de edad", esMayor);
+if (numero > 0) {
+  console.log("El número es positivo");
+} else if (numero < 0) {
+  console.log("El número es negativo");
+} else {
+  console.log("El número es cero");
+}*/
+
+/*let numero = Number(prompt("Ingresá un número:"));
+
+if (numero % 2 === 0) {
+  console.log("El número es par");
+} else {
+  console.log("El número es impar");
+}*/
+
+let nota = Number(prompt("Ingrese su nota:"));
+
+if (nota >= 7) {
+  console.log("Aprobado");
+} else if ((nota >= 4) & (nota <= 6)) {
+  console.log("Regular");
+} else if (nota < 4) {
+  console.log("Desaprobado");
+}
